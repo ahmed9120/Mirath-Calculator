1f0eb670220368bbde6d35ddd08e12caf464dac2
ahmedg120 <ahmed_20210070@fci.helwan.edu.eg>
2024-08-12 03:51:16 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+//
+//
+document.querySelector("#myForm").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  let myMoney = parseInt(document.querySelector("#money").value);
+  let haveWife = document.querySelector("#haveWife").checked;
+  let sonsNum = parseInt(document.querySelector("#sons").value);
+  let daughtersNum = parseInt(document.querySelector("#daughters").value);
+
+  let resultsDiv = document.querySelector("#results");
+  resultsDiv.innerHTML = "";
+
+  if (haveWife) {
+    let wifeShare = myMoney / 8;
+    myMoney -= wifeShare;
+    resultsDiv.innerHTML += `Wife has to take ${wifeShare} Pounds.<br>`;
+  }
+
+  let unitShare = myMoney / (sonsNum * 2 + daughtersNum);
+  let sonShare = unitShare * 2;
+  let daughterShare = unitShare;
+
+  if (sonsNum != 0) {
+    resultsDiv.innerHTML += `Each son should take ${sonShare} Pound <br>`;
+  }
+  if (daughtersNum != 0) {
+    resultsDiv.innerHTML += `Each daughter should take ${daughterShare} Pound<br>`;
+  }
+});
